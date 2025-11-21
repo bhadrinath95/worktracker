c35b0e9ceadca680686173b6227fbbb5ba9062b3
bhadrinath95 <bhadrinath95@gmail.com>
2025-11-21 21:20:30 +0530

    first commit

--- tracker/views.py
+++ tracker/views.py
@@ -107,7 +107,7 @@ class UpdateListView(LoginRequiredMixin, View):
     def get(self, request, task_id):
         task = get_object_or_404(Task, pk=task_id)
 
-        checkbox_updates = task.updates.filter(is_check_box=True)
+        checkbox_updates = task.updates.filter(is_check_box=True).order_by('name')
         normal_updates   = task.updates.filter(is_check_box=False).order_by('-date')
 
         form = UpdateForm()
